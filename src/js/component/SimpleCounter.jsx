350ff529e659d7c6df8e3ac6927ed1fb64ccb49b
Daniela Perdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-09-21 00:54:13 +0000

    Funcionalidad del contador

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -5,16 +5,17 @@ import '../../styles/SimpleCounter.css'
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const SimpleCounter = () => {
+const SimpleCounter = (props) => {
+
 	return (
 		<>
             <h1>Simple Counter</h1>
             <div className="counter-display">
                 <div className="clock-img"><i className="iconClock fa-regular fa-clock fa-beat-fade"></i></div>
-                <div className="seconds-thousands">0</div>
-                <div className="seconds-houndreds">0</div>
-                <div className="seconds-tens">0</div>
-                <div className="seconds-ones">0</div>
+                <div className="seconds-thousands">{props.thousandsDigit}</div>
+                <div className="seconds-houndreds">{props.houndredsDigit}</div>
+                <div className="seconds-tens">{props.tensDigit}</div>
+                <div className="seconds-ones">{props.onesDigit}</div>
             </div>
 		</>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SimpleCounter from "./SimpleCounter.jsx";
 
 //include images into your bundle
@@ -8,11 +8,32 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [counter, setCounter] = useState(0);
 
-	
+	useEffect(() => {
+		const interval = setInterval( () => {
+			setCounter(counter => counter + 1 )
+		}, 1000)
+		console.log(counter)
+
+		return () => clearInterval(interval)
+	}, [counter])
+
+	function calculateSeconds(aCounter, placeValue) {
+		return Math.floor(aCounter / placeValue) % 10
+	}
+
+
+
 	return (
 		
 		<div className="">
-			<SimpleCounter />
+
+			<SimpleCounter 
+			thousandsDigit = {calculateSeconds(counter, 1000)}
+			houndredsDigit = {calculateSeconds(counter, 100)}
+			tensDigit = {calculateSeconds(counter, 10)}
+			onesDigit = {calculateSeconds(counter, 1)}
+			/>
+			
 		</div>
 	);
 };
